c2829b4c99ec30484ff86e51c63b96a2d6e0ff94
Ravjot Singh <ravjotsingh@Ravjots-MacBook-Pro.local>
2025-02-21 19:40:57 -0500

    added multiple workflows

--- infrastructure/terraform/1-infrastructure/providers.tf
+++ infrastructure/terraform/1-infrastructure/providers.tf
@@ -4,10 +4,10 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 3.0"
     }
-  }
-  kubernetes = {
-    source  = "hashicorp/kubernetes"
-    version = "~> 2.0"
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0"
+    }
   }
 
   backend "azurerm" {}
